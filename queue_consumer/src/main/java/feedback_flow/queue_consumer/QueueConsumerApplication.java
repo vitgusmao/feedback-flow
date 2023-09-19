67e20f23626783f04514bb99cb3be9f3641079a4
Vitor Loura <vitorloura@gmail.com>
2023-09-19 10:49:59 -0300

    [] Adicionando configurações iniciais - Configurações da IDE - Configurações do projeto de API dos feedbacks - Configurações do projeto consumidor de feedbacks

---
+++ queue_consumer/src/main/java/feedback_flow/queue_consumer/QueueConsumerApplication.java
@@ -0,0 +1,13 @@
+package feedback_flow.queue_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueueConsumerApplication.class, args);
+    }
+
+}
