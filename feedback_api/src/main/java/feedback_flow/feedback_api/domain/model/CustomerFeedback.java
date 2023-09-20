5d6248b7f804cfeaf7fb6781d383fc7b3016e1e0
Mateus Simão <mateus.david@outlook.com>
2023-09-20 08:38:33 -0300

    Criando estrutura inicial do CustomerFeedback (#2)
    * feat - criando o model CustomerFeedback
    
    * libs - adicionando as bibliotecas iniciais
    
    * config - adicionando as configurações iniciais do sqs e h2
    
    * feat - criando o controller
    
    * feat - criando o repository
    
    * feat - criando o service

---
+++ feedback_api/src/main/java/feedback_flow/feedback_api/domain/model/CustomerFeedback.java
@@ -0,0 +1,28 @@
+package feedback_flow.feedback_api.domain.model;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class CustomerFeedback {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 250, nullable = false)
+    private String message;
+
+    private CustomerFeedbackType type;
+
+    private CustomerFeedbackStatus status;
+
+    @Override
+    public String toString() {
+        return "";
+    }
+}
---
+++ feedback_api/src/main/java/feedback_flow/feedback_api/domain/model/CustomerFeedbackType.java
@@ -0,0 +1,7 @@
+package feedback_flow.feedback_api.domain.model;
+
+public enum CustomerFeedbackType {
+    SUGGESTION,
+    COMPLIMENT,
+    CRITIQUE
+}
