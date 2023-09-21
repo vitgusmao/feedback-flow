f41f4dc3cb020b0abc08a0e8dfebfc03d9cd1ebe
mateus simão <mateus.david@outlook.com>
2023-09-21 20:32:44 -0300

    refactor - criando busca do customer feedback no repository

--- feedback_api/src/main/java/feedback_flow/feedback_api/application/repositories/CustomerFeedbackRepository.java
+++ feedback_api/src/main/java/feedback_flow/feedback_api/application/repositories/CustomerFeedbackRepository.java
@@ -1,8 +1,10 @@
 package feedback_flow.feedback_api.application.repositories;
 
+import feedback_flow.feedback_api.domain.customerFeedback.CustomerFeedback;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import feedback_flow.feedback_api.domain.customerFeedback.CustomerFeedback;
+import java.util.Optional;
 
 public interface CustomerFeedbackRepository extends JpaRepository<CustomerFeedback, Long> {
+    Optional<CustomerFeedback> findCustomerFeedbackById(Long id);
 }
