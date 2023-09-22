ad5e53c4b1105b25128da9e30ec51b0100e927d5
mateus simão <mateus.david@outlook.com>
2023-09-22 20:32:11 -0300

    refactor - utilizando componentes criados para exibir os feedback

--- feedback_client/src/main.jsx
+++ feedback_client/src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './components/App'
 import './index.css'
+import Topic from './components/Topic'
+import Container from '@mui/material/Container'
+import { Card } from '@mui/material'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Container>
+        <Card sx={{padding: 5}}>
+            <Topic />
+        </Card>
+    </Container>
 )
